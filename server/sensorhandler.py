e1acef84a91cf42c3c25eb3570d73b6853f8d9e7
hija <hilko.j@uni-muenster.de>
2018-10-09 03:07:07 +0200

    Added form to add a sensor

--- server/sensorhandler.py
+++ server/sensorhandler.py
@@ -4,28 +4,40 @@ from flask import (
 
 from server.db import get_db
 from server.util import sensor_exists
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 bp = Blueprint('sensors', __name__, url_prefix='/sensors')
 
+### FORMS ####
+class AddForm(FlaskForm):
+
+    def valid_sensorname(form, field):
+        import re
+        from wtforms import ValidationError
+        if re.search('[^a-z0-9_]+', field.data) is not None:
+            raise ValidationError('Sensorname must be lowercase and underscores only!')
+
+    name = StringField('Sensorname', validators=[DataRequired(), valid_sensorname])
+    description = StringField('Description', validators=[DataRequired()])
+##############
+
 @bp.route('/add', methods=('GET', 'POST'))
 def addsensor():
-    if request.method == 'POST':
-        if 'sensorname' in request.form and 'description' in request.form:
-            sensorname = request.form['sensorname']
-            if '/' in sensorname:
-                return 'Invalid sensorname'
-            description = request.form['description']
-            db = get_db()
-            if sensor_exists(db, sensorname):
-                return 'sensor already exists!'
-            db.execute(
-            'INSERT INTO sensor (sensorname, description) VALUES (?, ?)',
-            (sensorname, description))
-            db.commit()
-            return 'Added sensor!'
+    form = AddForm()
+    if form.validate_on_submit():
+        db = get_db()
+        if sensor_exists(db, form.name.data):
+            flash(u'There is already a sensor with this name in the database!', 'error')
         else:
-            return 'Missing parameters!'
-    return 'This method requires an POST-Request with the fields sensorname and description'
+            db.execute(
+                     'INSERT INTO sensor (sensorname, description) VALUES (?, ?)',
+                     (form.name.data, form.description.data))
+            db.commit()
+            flash(u'The sensor {} has been sucessfully added to the database!'.format(form.name.data), 'success')
+            return redirect(url_for('sensors.addsensor'))
+    return render_template('sensor/add.html', form=form)
 
 @bp.route('/')
 def getsensors():
