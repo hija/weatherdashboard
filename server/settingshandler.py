5daa87ecd88dd8ecd46285fce358a603c817ec2b
hija <hilko.j@uni-muenster.de>
2018-10-10 14:05:47 +0200

    Added Dummy Data Generation for Sensors (ToDo: Records)

--- server/settingshandler.py
+++ server/settingshandler.py
@@ -14,3 +14,37 @@ def getsensors():
 @bp.route('/about')
 def getabout():
     return render_template('settings/about.html')
+
+@bp.route('/reset_db', methods=("POST",))
+def reset_db():
+    from server.db import init_db
+    init_db()
+    flash('The database has been resetted!', 'success')
+    return render_template('settings/default.html')
+
+@bp.route('/generate_dummy_data', methods=("POST",))
+def generate_dummy_data():
+    from secrets import choice
+    db = get_db()
+
+    def generate_dummy_sensors():
+        names = ['alan', 'adam', 'collin', 'ethan', 'frank', 'homer']
+        rooms = ['garage', 'livingroom', 'kitchen', 'bath']
+        extender = ['final', 'new', 'old']
+        generated_sensors = []
+        for i in range(10):
+            generated_sensor = "{}_{}".format(choice(names), choice(rooms))
+            while generated_sensor in generated_sensors:
+                generated_sensor = "{}_{}".format(generated_sensor, choice(extender))
+            generated_sensors.append(generated_sensor)
+        for sensor in generated_sensors:
+            db.execute('INSERT INTO sensor (sensorname, description) VALUES (?, "Dummy Sensor!")', (sensor, ))
+        db.commit()
+        return generated_sensors
+
+    def generate_dummy_entries(sensors):
+        for _ in range(10):
+            pass
+    sensors = generate_dummy_sensors()
+    flash('The dummy entries have been generated', 'success')
+    return render_template('settings/default.html')
