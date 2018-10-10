c1a6f6e3caa32ff4d537516066364791cd947b9a
hija <hilko.j@uni-muenster.de>
2018-10-11 00:30:07 +0200

    Added first visualizations

--- server/vishandler.py
+++ server/vishandler.py
@@ -3,10 +3,32 @@ from flask import (
 )
 
 from server.db import get_db
-from server.util import sensor_exists
+from server.util import sensor_exists, get_sensor_id
 
 bp = Blueprint('visualiations', __name__, url_prefix='/visualisations')
 
 @bp.route('/')
 def getsensors():
     return render_template('vis/default.html')
+
+@bp.route('/show/<string:sensorname>')
+def get_vis_for_sensor(sensorname):
+    return get_vis_for_sensor_and_limit(sensorname, 3)
+
+@bp.route('/show/<string:sensorname>/<int:limit>')
+def get_vis_for_sensor_and_limit(sensorname, limit):
+    db = get_db()
+    if sensor_exists(db, sensorname):
+        records = db.execute(
+            'SELECT timepoint, temperature, humidity, pressure FROM record WHERE sensor_id = ? ORDER BY timepoint DESC LIMIT ?',
+            (get_sensor_id(db, sensorname), limit)
+        ).fetchall()
+        data = dict()
+        data['temperature'] = [record['temperature'] for record in records]
+        data['humidity'] = [record['humidity'] for record in records]
+        data['pressure'] = [record['pressure'] for record in records]
+        data['dates'] = [record['timepoint'] for record in records]
+        return render_template('vis/sensor_vis.html', data=data)
+    else:
+        flash('The sensor does not exist!', 'error')
+        return render_template('vis/default.html')
